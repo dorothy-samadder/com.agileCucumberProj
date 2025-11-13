38ac9dcec1a298858df5816e537fa95727a5c556
dorothy <dorothy.samadder@gmail.com>
2025-11-13 16:30:31 -0500

    first project commit to git

---
+++ src/test/java/Stepdefs/EnrollStepDefs.java
@@ -0,0 +1,144 @@
+package Stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EnrollStepDefs {
+
+	
+
+@When("I click Enroll Now on Home page")
+public void i_click_enroll_now_on_home_page() {
+    
+}
+
+@When("I navigate to Enroll page")
+public void i_navigate_to_enroll_page() {
+    
+}
+
+@Then("I verify the page title is {string}")
+public void i_verify_the_page_title_is(String string) {
+    
+}
+
+@Then("the Course Enrollment Form is displayed")
+public void the_course_enrollment_form_is_displayed() {
+    
+}
+
+@Given("I am on Enroll page")
+public void i_am_on_enroll_page() {
+   
+}
+
+@When("I enter First Name")
+public void i_enter_first_name() {
+    
+}
+
+@When("I enter Last Name")
+public void i_enter_last_name() {
+    
+}
+
+@When("I enter Phone Number")
+public void i_enter_phone_number() {
+    
+}
+
+@When("I enter Street Address")
+public void i_enter_street_address() {
+    
+}
+
+@When("I enter City")
+public void i_enter_city() {
+    
+}
+
+@When("I enter State")
+public void i_enter_state() {
+    
+}
+
+@When("I enter Postal Code")
+public void i_enter_postal_code() {
+    
+}
+
+@When("I select Country")
+public void i_select_country() {
+    
+}
+
+@When("I enter Email")
+public void i_enter_email() {
+   
+}
+
+@When("I enter Password")
+public void i_enter_password() {
+    
+}
+
+@When("I enter Confirm Password")
+public void i_enter_confirm_password() {
+  
+}
+
+@When("I select Course")
+public void i_select_course() {
+   
+}
+
+@When("I select Terms & Conditions checkbox")
+public void i_select_terms_conditions_checkbox() {
+   
+}
+
+@When("I click on Enroll Now button")
+public void i_click_on_enroll_now_button() {
+    
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
